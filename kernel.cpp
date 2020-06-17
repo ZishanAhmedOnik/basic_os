8190d0273589dc20706fa151ba59e66583c3bfcd
ONIK <zisanik@gmail.com>
2020-06-16 20:51:08 -0500

    fix calling global obj constructor bug

--- kernel.cpp
+++ kernel.cpp
@@ -11,6 +11,18 @@ void printf(const int8_t* str)
     }
 }
 
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+
+extern "C" void callConstructors()
+{
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+    {
+        (*i)();
+    }
+}
+
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
     printf("Hello VM World! with types :/");
